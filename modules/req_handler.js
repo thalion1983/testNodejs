b530df033aa953f52d95e19afb07eaecc5cd2b04
Leonardo Astros <leonardoastros@gmail.com>
2023-12-19 11:37:58 -0400

    [ADD] Added parse URL query and get parameters

--- modules/req_handler.js
+++ modules/req_handler.js
@@ -1,5 +1,8 @@
+var url = require('url');
+
 exports.handler = function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write('Sender url: '.concat(req.url));
+	var q = url.parse(req.url, true).query
+	res.write('param 1: '.concat(q.param1).concat('  param 2: ').concat(q.param2));
 	res.end();
 }
